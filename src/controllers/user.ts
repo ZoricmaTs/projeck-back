e47b3be4eb0f5f01ab5db10eca5c644075512954
zori <tszzzch@gmail.com>
2026-02-21 04:48:19 +0600

    [master]-query builder

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,34 +1,27 @@
 import type { Request, Response } from "express";
 import { userService } from "../services/user.ts";
 import { ApiError } from "../errors/api-error.ts";
+import { prisma } from "../lib/prisma.ts";
+import {buildUserQuery, type UserQueryParams} from '../utils/query-builder.js';
 
 type DeleteUserParams = {
   id: string;
 };
 
-const allowedSortFields = ["createdAt", "name", "email"] as const;
-const allowedOrders = ["asc", "desc"] as const;
-
 export const userController = {
-  async getUsers(req: Request, res: Response) {
-    const page = Number(req.query.page) || 1;
-    const limit = Number(req.query.limit) || 10;
+  async getUsers(req: { query: UserQueryParams; }, res: { json: (arg0: { data: { name: string; email: string; id: string; createdAt: Date; }[]; meta: { total: number; }; }) => void; }) {
+    const queryOptions = buildUserQuery(req.query);
+    const users = await prisma.user.findMany(queryOptions);
+    const total = await prisma.user.count({
+      where: queryOptions.where,
+    });
 
-    const sort = allowedSortFields.includes(req.query.sort as any)
-      ? (req.query.sort as any)
-      : "createdAt";
-
-    const order = allowedOrders.includes(req.query.order as any)
-      ? (req.query.order as any)
-      : "desc";
-
-    const search = req.query.search
-      ? String(req.query.search)
-      : undefined;
-
-    const result = await userService.getAll({page, limit, sort, order, search});
-
-    res.json(result);
+    res.json({
+      data: users,
+      meta: {
+        total,
+      },
+    });
   },
 
   async createUser(req: Request, res: Response) {
